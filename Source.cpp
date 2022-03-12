e11370dbb181bd823ea10d77947b618015c55631
NotSlater <59234115+NotSlater@users.noreply.github.com>
2022-03-12 05:46:30 +0000

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,5 +1,5 @@
 Sike bet you thought
-(5 Stars to Release Program)
+(15 Stars to Release Program)
 (20 Stars to Release Source)
 (30 Stars to Release AIOSpoofer)
 (50 Stars to Release Spoofer Source)
